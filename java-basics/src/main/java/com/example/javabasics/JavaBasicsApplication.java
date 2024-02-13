fadad450bcb499bac92339ab325fcb9b9c705991
tjaz <spegel.tjaz15@gmail.com>
2024-02-13 09:10:06 +0100

    JavaBasics - First commit

---
+++ java-basics/src/main/java/com/example/javabasics/JavaBasicsApplication.java
@@ -0,0 +1,13 @@
+package com.example.javabasics;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaBasicsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JavaBasicsApplication.class, args);
+    }
+
+}
